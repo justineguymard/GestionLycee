c81f34da34c09e1475f600b0f46cdcc181a75052
Sarra REMMACHE <sarraa.remmache@gmail.com>
2020-05-28 16:46:40 +0200

    modif entities

--- src/fr/inti/entities/Matiere.java
+++ src/fr/inti/entities/Matiere.java
@@ -4,5 +4,58 @@ public class Matiere {
 	
 	private int id;
 	private String intitule;
+	
+	// transformation UML en java 
+	private Professeur professeur;
+
+	//declaration des constructeurs 
+	
+	public Matiere() {
+		super();
+	}
+
+	public Matiere(String intitule) {
+		super();
+		this.intitule = intitule;
+	}
+
+	public Matiere(int id, String intitule) {
+		super();
+		this.id = id;
+		this.intitule = intitule;
+	}
+
+	// declaration des getters et setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIntitule() {
+		return intitule;
+	}
+
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+
+	public Professeur getProfesseur() {
+		return professeur;
+	}
+
+	public void setProfesseur(Professeur professeur) {
+		this.professeur = professeur;
+	}
+
+	// redefinition de la methode toString
+	@Override
+	public String toString() {
+		return "Matiere [id=" + id + ", intitule=" + intitule + "]";
+	}
+	
+	
 
 }
--- src/fr/inti/entities/Professeur.java
+++ src/fr/inti/entities/Professeur.java
@@ -1,16 +1,92 @@
 package fr.inti.entities;
 
 public class Professeur {
-	
-	//declaration des attributs
+
+	// declaration des attributs
 	private int id;
 	private String nom;
 	private String prenom;
 	private String dateAff;
-	
- //	trnasformation UML en java
-	
+
+	// transformation UML en java
+
 	private Matiere matiere;
 	private Departement department;
+
+	// declaration des constructeurs
+
+	public Professeur() {
+		super();
+	}
+
+	public Professeur(String nom, String prenom, String dateAff) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateAff = dateAff;
+	}
+
+	public Professeur(int id, String nom, String prenom, String dateAff) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateAff = dateAff;
+	}
+
+	// declaration des getters et setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getDateAff() {
+		return dateAff;
+	}
+
+	public void setDateAff(String dateAff) {
+		this.dateAff = dateAff;
+	}
+
+	public Matiere getMatiere() {
+		return matiere;
+	}
+
+	public void setMatiere(Matiere matiere) {
+		this.matiere = matiere;
+	}
+
+	public Departement getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Departement department) {
+		this.department = department;
+	}
+
+	// redefinition de la methode toString
+	@Override
+	public String toString() {
+		return "Professeur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", dateAff=" + dateAff + "]";
+	}
 
 }
